32af3a29954863a2b20cdec0615ab7a22ec9adc3
suganthiv18 <143151933+suganthiv18@users.noreply.github.com>
2023-10-08 13:29:16 -0400

    test

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,27 +1,24 @@
 package Runner;
 
-import org.junit.runner.RunWith;
-
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 
-import Utilities.ConfigReader;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import com.beust.jcommander.Parameters;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",
-glue={"StepDefinitions","Hooks"},
-plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"html:Reports/Cucumber-Reports/Reports.html"},
+@CucumberOptions(plugin = { "pretty","html:target/dsAlgoReport.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, // reporting purpose
+monochrome = false, // console output
+tags = "", // tags from feature file
+features = { "src/test/resources/features" }, // location of feature files
+glue = { "StepDefinitions", "Hooks" }) // location of step definition files
 
-dryRun=false,
-monochrome = true)
-
-public class TestRunner{
+public class TestRunner extends AbstractTestNGCucumberTests {
+	//@Parameters({"browser","url"})
+	@DataProvider(parallel = false)
+	public Object[][] scenarios() {
+	
+		return super.scenarios();
+		}
 
 }
